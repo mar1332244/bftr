11279c6ed0768a70648f3dbf2932fae266bb1b30
mar1332244 <marcelo1332244@gmail.com>
2022-07-28 03:38:18 +0000

    Implemented -w flag

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -11,10 +13,36 @@ const (
 	sub10 = ">%s[-<---------->]<"
 )
 
+type Flags struct {
+	maxWidth int
+	outFile  string
+}
+
+func parseFlags() Flags {
+	var f Flags
+	flag.IntVar(&f.maxWidth, "w", -1, "sets the max width of a given line of output")
+	flag.StringVar(&f.outFile, "o", "stdout", "filepath to write to the output to")
+	flag.Parse()
+	return f
+}
+
+func printOutput(output string, f Flags) {
+	if f.maxWidth <= 0 {
+		fmt.Println(output)
+		return
+	}
+	buffer := bytes.NewBufferString(output)
+	for 0 < buffer.Len() {
+		line := buffer.Next(f.maxWidth)
+		fmt.Printf("%s\n", line)
+	}
+}
+
 func toBrainfuck(input string) string {
 	var builder strings.Builder
 	var last byte
-	for _, char := range []byte(input) {
+	for i := 0; i < len(input); i++ {
+		char := input[i]
 		delta := int(char) - int(last)
 		loops := delta / 10
 		extra := delta % 10
@@ -27,20 +55,23 @@ func toBrainfuck(input string) string {
 			loop = sub10
 		}
 		if 0 < loops {
-			fmt.Fprintf(&builder, loop, strings.Repeat("+", loops))
+			loop = fmt.Sprintf(loop, strings.Repeat("+", loops))
+			builder.WriteString(loop)
 		}
-		fmt.Fprint(&builder, strings.Repeat(symbol, extra), ".")
+		builder.WriteString(strings.Repeat(symbol, extra))
+		builder.WriteByte('.')
 		last = char
 	}
 	return builder.String()
 }
 
 func main() {
-	if len(os.Args) < 2 {
+	f := parseFlags()
+	if flag.NArg() < 1 {
 		fmt.Fprintln(os.Stderr, "no input provided")
 		os.Exit(1)
 	}
-	input := os.Args[1]
+	input := flag.Arg(0)
 	output := toBrainfuck(input)
-	fmt.Println(output)
+	printOutput(output, f)
 }
